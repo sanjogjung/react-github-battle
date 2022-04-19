13d0a8a3f43c621750eb32f3dd6d4ad4c7b60080
Sanjog gc <gcsanjog@gmail.com>
2022-04-20 00:02:18 +0545

    code splitting for different routes

--- app/index.js
+++ app/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
-import Battle from './components/Battle';
-import Popular from './components/Popular';
 import './index.css'
 import { ThemeProvider, ThemeConsumer } from './contexts/theme'
 import Nav from './components/Nav';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-import Results from './components/Results';
 import { Switch } from 'react-router-dom'
+import Loading from './components/Loading';
+
+const Popular = React.lazy(() => import("./components/Popular"))
+const Battle = React.lazy(() => import('./components/Battle'))
+const Results = React.lazy(() => import("./components/Results"))
 
 class App extends React.Component {
       state = {
@@ -26,13 +28,14 @@ class App extends React.Component {
           <div className={this.state.theme}>
             <div className='container'>
               <Nav />
-
+              <React.Suspense fallback={<Loading />}>
               <Switch>
                 <Route exact path="/" component={Popular}/>
                 <Route exact path="/battle" component={Battle} />
                 <Route path="/battle/results" component={Results}/>
                 <Route render={()=> <h1>404</h1>}/>
               </Switch>
+              </React.Suspense>
             </div>
           </div>
         </ThemeProvider>
